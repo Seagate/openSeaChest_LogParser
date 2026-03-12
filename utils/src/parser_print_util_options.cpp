36d1da42426f3adb83bf851a467e046db7be6afe
Tim <Timothy.O.Gilmer@seagate.com>
2026-03-12 11:50:05 -0600

    removed code

--- utils/src/parser_print_util_options.cpp
+++ utils/src/parser_print_util_options.cpp
@@ -1053,12 +1053,7 @@ void CPrintProm::parseJSONToProm(JSONNODE* nData, std::string inserialNumber, js
                 JSONNODE_ITERATOR it_jsonArrayNode = json_begin(*it_jsonArray);
                 json_char *jsonArrayNodeName = json_name(*it_jsonArrayNode);
                 // Do nothing if the node is NULL
-                //if (jsonArrayNodeName == M_NULLPTR)
-                //{
-                //    json_free(jsonArrayNodeName);
-                //    json_free(jsonArrayName);
-                //    break;
-                //}
+
                 // Run this method recursively
                 parseJSONToProm(*it_jsonArray, inserialNumber, jsonArrayNodeName);
                 // Clear pointers
